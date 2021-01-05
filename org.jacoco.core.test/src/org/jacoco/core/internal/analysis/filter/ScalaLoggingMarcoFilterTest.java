81ded62bfecf0f5a54788ee776d182e14f9669b8
Gergely Fábián <gergo.fb@gmail.com>
2021-01-05 15:47:52 +0100

    Filter out macros for com.typesafe.scalalogging

---
+++ org.jacoco.core.test/src/org/jacoco/core/internal/analysis/filter/ScalaLoggingMarcoFilterTest.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2021 Mountainminds GmbH & Co. KG and Contributors
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Gergely Fábián - initial API and implementation
+ *
+ *******************************************************************************/
+package org.jacoco.core.internal.analysis.filter;
+
+import org.jacoco.core.internal.instr.InstrSupport;
+import org.junit.Test;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.MethodNode;
+
+/**
+ * Unit tests for {@link SyntheticFilter}.
+ */
+public class ScalaLoggingMarcoFilterTest extends FilterTestBase {
+
+	private final ScalaLoggingMacroFilter filter = new ScalaLoggingMacroFilter();
+
+	@Test
+	public void testNonScala() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				"name", "()V", null, null);
+		m.visitInsn(Opcodes.NOP);
+
+		filter.filter(m, context, output);
+
+		assertIgnored();
+	}
+
+	@Test
+	public void testScalaNop() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				"name", "()V", null, null);
+		context.classAttributes.add("Scala");
+
+		m.visitInsn(Opcodes.NOP);
+
+		filter.filter(m, context, output);
+
+		assertIgnored();
+	}
+
+	@Test
+	public void testScalaLoggingMacroRemoved() {
+		final MethodNode m = new MethodNode(InstrSupport.ASM_API_VERSION, 0,
+				"whatever", "()Z", null, null);
+		context.classAttributes.add("Scala");
+
+		final Label label = new Label();
+
+		final Range range = new Range();
+
+		m.visitInsn(Opcodes.NOP);
+		m.visitInsn(Opcodes.NOP);
+		m.visitMethodInsn(Opcodes.INVOKEVIRTUAL,
+				"com/typesafe/scalalogging/Logger", "underlying",
+				"()Lorg/slf4j/Logger;", false);
+		m.visitMethodInsn(Opcodes.INVOKEINTERFACE, "org/slf4j/Logger",
+				"isDebugEnabled", "()Z", true);
+		range.fromInclusive = m.instructions.getLast();
+
+		m.visitJumpInsn(Opcodes.IFEQ, label);
+		range.toInclusive = m.instructions.getLast();
+
+		m.visitInsn(Opcodes.NOP);
+
+		filter.filter(m, context, output);
+
+		assertIgnored(range);
+		assertNoReplacedBranches();
+	}
+
+}
